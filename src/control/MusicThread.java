add89b9dd2cd2a1b5a2fee5012d344b703315d73
Roman <RomanvonKlein@yahoo.de>
2016-03-04 13:57:52 +0100

    music

--- src/control/MusicThread.java
+++ src/control/MusicThread.java
@@ -12,29 +12,28 @@ public class MusicThread extends Thread
 	/**
 	 * This method initializes the soundtrack to be player.
 	 */
-	 public void run() 
+	 public void run()
 	 {
 	      try {
 	        Clip clip = AudioSystem.getClip();
-	        AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("/sounds/background/"+getRandomBackgroundMusic()));
+	        AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("/sounds/background/"+getRandomBackgroundMusic()));        
 	        clip.open(inputStream);
 	        clip.start();
-	        //sleep(clip.getMicrosecondLength());
+	        sleep(clip.getMicrosecondLength());
 	      } catch (Exception e) {
 	        System.err.println("Did not work :(");
 	      }
-	      
 	 }
 	 /**
 	  * Returns the name of a random backgroundmusic file.
+	  * (a random file from the /sounds/background/ folder)
 	  */
 	 private String getRandomBackgroundMusic()
 	 {
 		 Random rand = new Random();
-		 File folder = new File("/sounds/background/");
+		 File folder = new File("./"+"/sounds/background/");
 		 File[] listOfFiles = folder.listFiles();
-		 int random = rand.nextInt(listOfFiles.length);
-		 System.out.println("Looking for backgroundmusic at this path: '/sounds/background'. Found " + listOfFiles.length +" files.");
+	     int random = rand.nextInt(listOfFiles.length);
 		 return listOfFiles[random].getName();
 	 }
 }
